9a27c17735079478bbf6baa5d8ea74661966d9d7
Sindre <sindrelmjoes@gmail.com>
2026-05-14 20:24:07 +0200

    Add handle_mdns_event function
    Signed-off-by: Sindre <sindrelmjoes@gmail.com>

--- src/network/swarm.rs
+++ src/network/swarm.rs
@@ -11,7 +11,11 @@ use libp2p::{
   swarm::SwarmEvent,
 };
 
-use crate::network::texas_behaviour::TexasBehaviour;
+
+use crate::network::texas_behaviour::{
+    TexasBehaviour,
+    TexasBehaviourEvent,
+};
 
 pub async fn start() -> Result<(), Box<dyn Error>> {
   let mut swarm = libp2p::SwarmBuilder::with_new_identity()
@@ -43,13 +47,39 @@ pub async fn start() -> Result<(), Box<dyn Error>> {
   let peer_id = swarm.local_peer_id();
   println!("local peer id: {peer_id}");
 
-  swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+  swarm.listen_on("/ip4/192.168.0.167/tcp/0".parse()?)?;
 
   loop {
     match swarm.select_next_some().await {
-      SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
+      SwarmEvent::NewListenAddr { address, .. } => {
+        println!("Listening on {address:?}");
+      }
+      
+      SwarmEvent::Behaviour(TexasBehaviourEvent::Mdns(event)) => {
+        handle_mdns_event(event, &mut swarm.behaviour_mut().gossipsub);
+      }
+
       SwarmEvent::Behaviour(event) => println!("{event:?}"),
       _ => {}
+    }
+  }
+}
+
+
+fn handle_mdns_event(event: mdns::Event, gossipsub: &mut gossipsub::Behaviour) {
+  match event {
+    mdns::Event::Discovered(list) => {
+      for (peer_id, _addr) in list {
+        println!("mDNS discovered peer: {peer_id}");
+        gossipsub.add_explicit_peer(&peer_id);
+      }
+    }
+
+    mdns::Event::Expired(list) => {
+      for (peer_id, _addr) in list {
+        println!("Lost connection to peer: {peer_id}");
+        gossipsub.remove_explicit_peer(&peer_id);
+      }
     }
   }
 }
