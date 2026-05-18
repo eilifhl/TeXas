b292bf83a9973f806b1801eb55bc84fdf6afd137
eilifhl <eilifhl@stud.ntnu.no>
2026-05-18 13:11:14 +0200

    add subscribe/publish command channel for gossipsub

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -4,17 +4,27 @@ mod texas_behaviour;
 use anyhow::Result;
 use tokio::{runtime::Handle, sync::mpsc};
 
+pub const DEFAULT_DOCUMENT_TOPIC: &str = "texas/document/main";
+
 pub enum NetworkEvent {
     LocalPeerId(String),
     Listening(String),
     PeerDiscovered(String),
     PeerExpired(String),
+    Subscribed(String),
+    MessageReceived { topic: String, payload: String },
     Log(String),
     Error(String),
 }
 
+pub enum NetworkCommand {
+    Subscribe { topic: String },
+    Publish { topic: String, payload: Vec<u8> },
+}
+
 pub struct NetworkHandle {
     receiver: mpsc::UnboundedReceiver<NetworkEvent>,
+    command_sender: mpsc::UnboundedSender<NetworkCommand>,
 }
 
 impl NetworkHandle {
@@ -25,17 +35,38 @@ impl NetworkHandle {
         }
         events
     }
+
+    pub fn subscribe(&self, topic: impl Into<String>) -> Result<()> {
+        self.command_sender
+            .send(NetworkCommand::Subscribe {
+                topic: topic.into(),
+            })
+            .map_err(|error| anyhow::anyhow!("failed to queue subscribe command: {error}"))
+    }
+
+    pub fn publish(&self, topic: impl Into<String>, payload: Vec<u8>) -> Result<()> {
+        self.command_sender
+            .send(NetworkCommand::Publish {
+                topic: topic.into(),
+                payload,
+            })
+            .map_err(|error| anyhow::anyhow!("failed to queue publish command: {error}"))
+    }
 }
 
 pub fn start(runtime: &Handle) -> Result<NetworkHandle> {
-    let (sender, receiver) = mpsc::unbounded_channel();
-    let service_sender = sender.clone();
+    let (event_sender, receiver) = mpsc::unbounded_channel();
+    let (command_sender, command_receiver) = mpsc::unbounded_channel();
+    let service_sender = event_sender.clone();
 
     runtime.spawn(async move {
-        if let Err(error) = swarm::run(service_sender).await {
-            let _ = sender.send(NetworkEvent::Error(error.to_string()));
+        if let Err(error) = swarm::run(service_sender, command_receiver).await {
+            let _ = event_sender.send(NetworkEvent::Error(error.to_string()));
         }
     });
 
-    Ok(NetworkHandle { receiver })
+    Ok(NetworkHandle {
+        receiver,
+        command_sender,
+    })
 }
--- src/network/swarm.rs
+++ src/network/swarm.rs
@@ -4,11 +4,14 @@ use libp2p::{gossipsub, mdns, noise, ping, swarm::SwarmEvent, tcp, yamux};
 use tokio::sync::mpsc;
 
 use crate::network::{
-    NetworkEvent,
+    NetworkCommand, NetworkEvent,
     texas_behaviour::{TexasBehaviour, TexasBehaviourEvent},
 };
 
-pub async fn run(events: mpsc::UnboundedSender<NetworkEvent>) -> Result<()> {
+pub async fn run(
+    events: mpsc::UnboundedSender<NetworkEvent>,
+    mut commands: mpsc::UnboundedReceiver<NetworkCommand>,
+) -> Result<()> {
     let mut swarm = libp2p::SwarmBuilder::with_new_identity()
         .with_tokio()
         .with_tcp(
@@ -39,19 +42,42 @@ pub async fn run(events: mpsc::UnboundedSender<NetworkEvent>) -> Result<()> {
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
     loop {
-        match swarm.select_next_some().await {
-            SwarmEvent::NewListenAddr { address, .. } => {
-                let _ = events.send(NetworkEvent::Listening(address.to_string()));
+        tokio::select! {
+            swarm_event = swarm.select_next_some() => {
+                match swarm_event {
+                    SwarmEvent::NewListenAddr { address, .. } => {
+                        let _ = events.send(NetworkEvent::Listening(address.to_string()));
+                    }
+                    SwarmEvent::Behaviour(TexasBehaviourEvent::Mdns(event)) => {
+                        handle_mdns_event(event, &mut swarm.behaviour_mut().gossipsub, &events);
+                    }
+                    SwarmEvent::Behaviour(TexasBehaviourEvent::Gossipsub(event)) => {
+                        handle_gossipsub_event(event, &events);
+                    }
+                    SwarmEvent::Behaviour(event) => {
+                        let _ = events.send(NetworkEvent::Log(format!("{event:?}")));
+                    }
+                    _ => {}
+                }
             }
-            SwarmEvent::Behaviour(TexasBehaviourEvent::Mdns(event)) => {
-                handle_mdns_event(event, &mut swarm.behaviour_mut().gossipsub, &events);
+            command = commands.recv() => {
+                match command {
+                    Some(NetworkCommand::Subscribe { topic }) => {
+                        subscribe_topic(&mut swarm.behaviour_mut().gossipsub, &topic, &events)?;
+                    }
+                    Some(NetworkCommand::Publish { topic, payload }) => {
+                        publish_message(&mut swarm.behaviour_mut().gossipsub, &topic, payload, &events)?;
+                    }
+                    None => {
+                        let _ = events.send(NetworkEvent::Log("network command channel closed".to_owned()));
+                        break;
+                    }
+                }
             }
-            SwarmEvent::Behaviour(event) => {
-                let _ = events.send(NetworkEvent::Log(format!("{event:?}")));
-            }
-            _ => {}
         }
     }
+
+    Ok(())
 }
 
 fn handle_mdns_event(
@@ -71,6 +97,44 @@ fn handle_mdns_event(
                 gossipsub.remove_explicit_peer(&peer_id);
                 let _ = events.send(NetworkEvent::PeerExpired(peer_id.to_string()));
             }
+        }
+    }
+}
+
+fn subscribe_topic(
+    gossipsub: &mut gossipsub::Behaviour,
+    topic: &str,
+    events: &mpsc::UnboundedSender<NetworkEvent>,
+) -> Result<()> {
+    let topic = gossipsub::IdentTopic::new(topic);
+    gossipsub.subscribe(&topic)?;
+    let _ = events.send(NetworkEvent::Subscribed(topic.to_string()));
+    Ok(())
+}
+
+fn publish_message(
+    gossipsub: &mut gossipsub::Behaviour,
+    topic: &str,
+    payload: Vec<u8>,
+    events: &mpsc::UnboundedSender<NetworkEvent>,
+) -> Result<()> {
+    let topic = gossipsub::IdentTopic::new(topic);
+    gossipsub.publish(topic.clone(), payload)?;
+    let _ = events.send(NetworkEvent::Log(format!("published message on {}", topic)));
+    Ok(())
+}
+
+fn handle_gossipsub_event(event: gossipsub::Event, events: &mpsc::UnboundedSender<NetworkEvent>) {
+    match event {
+        gossipsub::Event::Message { message, .. } => {
+            let payload = String::from_utf8_lossy(&message.data).into_owned();
+            let _ = events.send(NetworkEvent::MessageReceived {
+                topic: message.topic.to_string(),
+                payload,
+            });
+        }
+        other => {
+            let _ = events.send(NetworkEvent::Log(format!("{other:?}")));
         }
     }
 }
